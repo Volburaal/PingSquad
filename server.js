f1d1760bb30fbe2e190f3f78f5b667ce9ea51b56
Sameed17 <76390629+Sameed17@users.noreply.github.com>
2024-12-08 01:13:44 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, { cors: { origin: "*" } });
+const io = socketIo(server, { cors: { origin: "*" } , maxHttpBufferSize: 1e8 });
 
 let peerMap = new Map();
 
@@ -17,39 +17,43 @@ app.get('/', (req, res) => {
   const version = "1.0.6";
   res.send(`Server is running v${version}`);
 });
-
+server.maxHttpBufferSize = 1e8;
 io.on('connection', (socket) => {
   const peerName = socket.handshake.query.name;
   console.log(`${peerName} (${socket.id}) has connected.`);
-  socket.broadcast.emit('peerJoined', peerName);
   peerMap.set(socket.id, peerName);
   io.emit('mapUpdate', Array.from(peerMap.entries()));
+  socket.emit('loadPast', Array.from(peerMap.entries()));
   console.log(peerMap);
+  socket.broadcast.emit('peerJoined', {id: socket.id, peerName});
   
   socket.on('disconnecting', (name) => {
     console.log('A peer disconnected:', socket.id);
+    console.log('name: ',peerMap.get(socket.id))
     const peerName = peerMap.get(socket.id);
     peerMap.delete(socket.id);
     io.emit('mapUpdate', Array.from(peerMap.entries()));
-    socket.broadcast.emit('peerLeft', peerName);
+    socket.broadcast.emit('peerLeft', {id: socket.id, peerName});
   });
 
   socket.on('chatMessage', (msg) => {
-    console.log(socket.id, " says: ", msg);
-    socket.broadcast.emit('chatMessage', { id: socket.id, msg });
+    console.log(socket.id, " says: ", msg.msg);
+    socket.broadcast.emit('chatMessage', { id: socket.id, ...msg });
   });
   socket.on('file-data', (payload) => {
     console.log(`Received file from ${socket.id}: ${payload.name}`);
     socket.broadcast.emit('file-received', { id: socket.id, ...payload });
   });
-  // Handle file chunks
+
+  // Handle file chunk transfer
   socket.on('file-chunk', (payload) => {
-    console.log('Server received file chunk:', payload);
+    console.log(`Received file chunk from ${socket.id}: ${payload.name}, Offset: ${payload.offset}`);
     socket.broadcast.emit('file-chunk', { id: socket.id, ...payload });
   });
+
   // Handle file transfer completion
   socket.on('file-complete', ({ name, size }) => {
-    console.log(`File transfer complete: ${name} (${size} bytes)`);
+    console.log(`File transfer complete from ${socket.id}: ${name} (${size} bytes)`);
     socket.broadcast.emit('file-complete', { id: socket.id, name });
   });
 
